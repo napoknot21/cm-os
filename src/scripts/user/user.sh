aecb6e7dcfa296dcaf09310c61e8ffa82756090b
napoknot21 <kinbrian5@hotmail.com>
2022-09-21 10:26:52 +0200

    Fixes and add of sudoers script

--- src/scripts/user/user.sh
+++ src/scripts/user/user.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
-echo -n "Enter a name to create a your user : "
-read user_name
+echo "[*] Creating your new user...\n"
 
+echo -n "[!] Enter a name to create a your user : "
+read user_name
 useradd -m $user_name
 
-echo -n "Enter your password"
-
+echo -n "\n[!] Enter your password: "
 passwd $user_name
 
-echo -n "Adding your user to groups..."
-
+echo -n "\n[*] Adding your user to groups..."
 usermod -aG wheel,storage,optical,audio,video $user_name
 
-echo -n "User succesfully added to your system !"
+echo -n "[*] User succesfully added to your system !"
+
+# Author @napoknot21
