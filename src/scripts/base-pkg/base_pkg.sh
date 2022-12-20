ef709ccae8c43ed72530e4d6364bff238ac289d0
napoknot21 <kinbrian5@hotmail.com>
2022-12-20 23:37:13 +0100

    starship update and add base pkg

--- src/scripts/base-pkg/base_pkg.sh
+++ src/scripts/base-pkg/base_pkg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pacman -S feh picom volumeicon udiskie cbatticon zip unzip mdcat ntfs-3g brightnessctl scrot redshift code vlc locate jdk-openjdk htop rofi papirus-icon-theme bat lsd exa
+pacman -S feh picom volumeicon udiskie cbatticon zip unzip mdcat ntfs-3g brightnessctl scrot redshift code vlc locate jdk-openjdk htop rofi papirus-icon-theme bat lsd exa ranger ocaml rust
