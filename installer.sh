aa8f25f72266d5b95fc502ac553c1119312cdb32
napoknot21 <napoknot21@protonmail.com>
2023-08-30 23:06:28 +0200

    desktop config installer update

--- installer.sh
+++ installer.sh
@@ -1,67 +1,125 @@
 #!/bin/sh
 
-echo -n "[?] Do you want to start the installation and set up? [y/n] : "
-read my_var
+echo -n "\n[?] Do you want to start the installation and set up? [y/n] : "
+read -p START_ANSWER
 
-if [[ $my_var = [Yy] ]];then
-    echo "[*] Starting installation...\n"
+
+check_internet()
+{
+  tool='curl'
+  tool_opts='-s --connect-timeout 8'
+
+  if ! $tool $tool_opts https://archlinux.org/ > /dev/null 2>&1; then
+    err "You don't have an Internet connection!"
+  fi
+
+  return $SUCCESS
+}
+
+
+#Script
+if [[ $START_ANSWER = [Yy] ]]; then
+    
+    check_internet
+
+    echo "[+] Starting installation...\n"
     sleep 1
+
+    #Variables for the script
+    CMOS_PATH=$PWD
+    CMOS_DIR_EXTRAS=$CMOS_PATH/extras/
+    CMOS_DIR_SCRIPTS=$CMOS_PATH/src/scripts/
+
+    # REQUIRED PACKAGES AND CONFIG
     
+    ### GIT check
     
-    # Local time and clock
-    echo "[*] Setting the local clock..."
-    ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
-    hwclock --systohc
+    GITCONFIG_FILE=$HOME/.gitconfig
+    
+    if [[ ! -f "$GITCONFIG_FILE" ]]; then
+
+        echo -e "\[!] .gitconfig does not exist. Creating one now..."
+
+        # Ask for the user's name and email
+        read -p "[?] Enter your name: " name_git
+        read -p "[?] Enter your email: " email_git
+
+        # Create the .gitconfig file and add name and email
+        git config --global user.name "$name_git"
+        git config --global user.email "$email_git"
+
+        echo -e "\N[*] .gitconfig has been created with the provided name and email."
+
+    else
+    
+        echo "\n[+] .gitconfig already exists. Continuing...\n"
+    
+    fi
+
+
     sleep 1
-    # TODO local-gen 
-    echo -e "[!] Done ! \n"
 
 
-    # Keymap and language config
-    echo "LANG=us_US.UTF-8" > /etc/locale.conf
-    echo "KEYMAP=fr" > /etc/vconsole.conf
-    echo "[?] Your Hostname (device's name): "
-    read hostname_device
-    echo $hostname_device > /etc/hostname
-    echo "[!] Done ! \n"
-    
-    # Network localhost
-    echo -e "\n" >> /etc/hosts
-    echo -e "127.0.0.1\tlocalhost" >> /etc/hosts
-    echo -e "::1\tlocalhost" >> /etc/hosts
-    echo -e "127.0.1.1\t$hostname_device.localhost\t$hostname_device" >> /etc/hosts
-    echo -e "[!] Done !\n" 
+    ### XORG installation
 
-    #installation of basic packages
-    pacman -S networkmanager network-manager-applet bluez bluez-utils pulseaudio pulseaudio-bluetooth alsa-utils dosfstools mtools cups
+    echo -e "\n[*] Let's install XORG server for graphics !\n"
 
-    # Start/Enbale basic services
-    systemctl enable NetworkManager
-    systemctl enable bluetooth
-    systemctl enable cups.service
+    sudo pacman -S xorg xorg-server xorg-xinit
 
+    echo -e "\n[*] X Server is installed successfully ! continuing...\n"
+
+
+    sleep 1
+
+
+    ### DEKSTOPS installation
+
+    dektops=("Qtile" "Bspwm" "Gnome" "KDE" "OpenBox" "Xmonad")
+
+    echo -e "\n[*] Dektop and window managers avaliables !\n"
     
-    # Permission for other scripts
-    # chmod +x ./src/scripts/user/user.sh
-    # chmod +x ./src/scripts/audio/audio.sh
-    # chmod +x ./src/scripts/black-arch/black-arch.sh
-    # TODO change permission for other scripts...    
+    for i in "${!options[@]}"; do
     
+        echo -e "\n[]"
+        echo "$((i+1))) ${options[i]}"
+        echo -e "\n"
+
+    done
+
+    read -p "[?] Enter a selection (default=all): " selection
+
+    # If nothing is entered, default to installing all packages
+    if [ -z "$selection" ]; then
+
+        selection=$(seq -s, 1 ${#options[@]})
     
-    # Setting up the user
-    echo -e "\n[*] Setting up your username... !\n"
-    echo "[?] Your username : "
-    read home_user
-    useradd -m $home_user
-    echo "\n[*] Enter 2 times your password !"
-    passwd $home_user
-    usermod -aG wheel,storage,video,audio,optical $home_user
+    fi
+
+    # Convert comma-separated list of numbers to array
+    IFS=',' read -ra selected_indices <<< "$selection"
+
+    # Install selected or all packages
+    for index in "${selected_indices[@]}"; do
+        
+        # Validate input
+        if [[ $index -ge 1 && $index -le ${#options[@]} ]]; then
+        
+            package=${options[$((index-1))]}
+            echo "\n[*] Installing $package..."
+            sudo pacman -S $package
+        
+        else
+
+            echo "Invalid option: $index"
+        
+        fi
     
-    
-    
+    done
+
 
 else
-	echo "[*] Exiting...!"
+
+    echo -e "\n[-] Installation aborted.\n"
+    exit 1
+
 fi
-
-
