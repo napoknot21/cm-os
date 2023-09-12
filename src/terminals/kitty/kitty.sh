ce3605ae49aae43cc9bd7a24d82e7cbe7f807f3a
napoknot21 <napoknot21@protonmail.com>
2023-09-12 19:59:34 +0200

    minor fixes

--- src/terminals/kitty/kitty.sh
+++ src/terminals/kitty/kitty.sh
@@ -12,7 +12,7 @@ echo -e "\n[*] Installing the package...\n"
 sudo pacman -S kitty
 
 # Config
-echo -e "\n[*] Setting up the emulator...\n"
+echo -e "\n[*] Setting up the Kitty terminal emulator...\n"
 
 # Check if directory exists, if it does, delete, otherwise create
 if [ -d "$DIR_KITTY" ]; then
@@ -25,12 +25,18 @@ fi
 mkdir -p $HOME/.config/kitty
 
 # Copy the config files
-if ! cp -rv ./src/* $HOME/.config/kitty/; then
+if ! cp -rv ./src/color.ini ./src/kitty.conf $HOME/.config/kitty/; then
 
-    echo -e "\n[-] Failed to copy Alacritty configuration. Aborting...\n"
+    echo -e "\n[-] Failed to copy kitty configuration. Aborting...\n"
     exit 1
 
 fi
+
+# Copy the prompt config
+if ! cp -rv ./src/.p10k.zsh $HOME/; then
+
+    echo -e "\n[-] Failed to copy kitty p10k configuration. Aborting...\n"
+    echo 1
 
 # Success message
 echo -e "\n[+] Kitty succesfully set up !\n"
