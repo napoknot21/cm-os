b5b5f2cd78e1d0929f3856e1aaa6ee01cdf40ed8
napoknot21 <napoknot21@protonmail.com>
2023-10-14 16:34:13 +0200

    Fixes for polybar and bspwm

--- src/desktops/bspwm/bspwm.sh
+++ src/desktops/bspwm/bspwm.sh
@@ -19,14 +19,14 @@ BSPWM_DIR=$1
 mkdir -p $HOME/.config/bspwm && mkdir -p $HOME/.config/sxhkd
 
 # Copying files
-if ! cp -rv $QBSPWM_DIR/src/bspwmrc $QBSPWM_DIR/src/scripts $HOME/.config/bspwm; then
+if ! cp -rv $BSPWM_DIR/src/bspwmrc $BSPWM_DIR/src/scripts $HOME/.config/bspwm/; then
 
     echo -e "\n[-] Failed to copy config files. Aborting...\n"
     exit 1
 
 fi
 
-if ! cp -rv $QBSPWM_DIR/src/sxhkdrc $HOME/.config/sxhkd; then
+if ! cp -rv $BSPWM_DIR/src/sxhkdrc $HOME/.config/sxhkd; then
 
     echo -e "\n[-] Failed to copy config files. Aborting...\n"
     exit 1
--- src/scripts/polybar/polybar.sh
+++ src/scripts/polybar/polybar.sh
@@ -14,11 +14,20 @@ POLYBAR_DIR=$1
 # Ensure the directory exists or create it
 mkdir -p $HOME/.config/polybar
 
+# Copy of the fonts
+if ! sudo cp $POLYBAR_DIR/src/fonts/* /usr/share/fonts/; then
+    
+    echo -e "\n[-] Failed to copy files to Polybar. Aborting...\n"
+    exit 1
+
+fi
+
 # Copy the config files
-if ! cp -v $POLYBAR_DIR/src/* $HOME/.config/polybar/; then
+if ! cp -vr $POLYBAR_DIR/src/* $HOME/.config/polybar/; then
 
     echo -e "\n[-] Failed to copy files to Polybar. Aborting...\n"
     exit 1
+
 fi
 
 # Success message
