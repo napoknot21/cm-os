6f035161a57f9e5915f5cc7e2a2c2b9a4247ffcc
napoknot21 <napoknot21@protonmail.com>
2025-02-02 02:16:26 +0100

    Missing font for polybar display

--- src/fonts/fonts.sh
+++ src/fonts/fonts.sh
@@ -1,3 +1,3 @@
-sudo pacman -S ttf-dejavu ttf-liberation noto-fonts awesome-terminal-fonts
+sudo pacman -S ttf-dejavu ttf-liberation awesome-terminal-fonts ttf-hack-nerd
 sudo pacman -S noto-fonts noto-fonts-emoji noto-fonts-cjk
 sudo pacman -S ttf-ubuntu-mono-nerd ttf-noto-nerd ttf-ubuntu-nerd ttf-jetbrains-mono-nerd
